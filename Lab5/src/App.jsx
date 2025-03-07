aaaafffe2a59e6d97e1f75da2c816e1fc37bab6c
MCP Server <mcp@example.com>
2025-03-06 19:52:14 -0600

    Fix imports and fallback route in App.jsx

---
+++ Lab5/src/App.jsx
@@ -0,0 +1,32 @@
+import { Routes, Route } from 'react-router-dom';
+
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import React from 'react'
+
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Perfil from './pages/Perfil'
+import Layout from './pages/Layout';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Routes</h1>
+      <Layout />
+      <Routes>
+        <Route path="Dashboard" element={<Dashboard />} />
+        <Route path="Home" element={<Home />} />
+        <Route path="Login" element={<Login />} />
+        <Route path="Perfil" element={<Perfil />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
